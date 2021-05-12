838693136e0753e3a7b5eb57ba84014fd392a489
venu gopal reddy <venu.gopal5506@gmail.com>
2021-05-12 19:44:00 +0530

    Spring core

---
+++ NewSpringCoreMVC/src/main/java/com/ojas/controller/FrontControl.java
@@ -0,0 +1,29 @@
+package com.ojas.controller;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.ojas.MvcConfig;
+
+public class FrontControl extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	 @Override
+	    protected Class<?>[] getRootConfigClasses() {
+	        // TODO Auto-generated method stub
+	        return new Class[] {MvcConfig.class};
+	    }
+
+	 
+	    @Override
+	    protected Class<?>[] getServletConfigClasses() {
+	        // TODO Auto-generated method stub
+	        return null;
+	    }
+ 
+
+	    @Override
+	    protected String[] getServletMappings() {
+	        // TODO Auto-generated method stub
+	        return new String[] {"/"};
+	    }
+	    
+}
